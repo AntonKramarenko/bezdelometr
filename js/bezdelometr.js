ffed091c62c16d024d818762bf7a1abf02b7841c
AntonKramarenko <kramarenko4362@gmail.com>
2020-05-13 18:31:52 +0300

    initial commit

---
+++ js/bezdelometr.js
@@ -0,0 +1,7 @@
+const tapOnBtn = document.querySelector('.tap_btn')
+const resultTapNumber = document.querySelector('.tap_number')
+
+function tap() {
+    resultTapNumber =0
+    resultTapNumber.textContent = resultTapNumber+1;
+}
